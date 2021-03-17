556065b3ac9fcdab30be8d83bf42565fa29b51bd
dindamajesty13 <dindamesti@gmail.com>
2021-03-17 14:59:42 +0700

    add function testing

--- test_app.py
+++ test_app.py
@@ -19,6 +19,19 @@ class TestApp(unittest.TestCase):
         print(hasiltestingsemua)
         ambilsatuhasiltesting = hasiltestingsemua[0]
         self.assertLessEqual(ambilsatuhasiltesting, 1)
-        
 
+    def test_02_DindaMajesty_1184011(self):
+        from Chapter01.DindaMajesty1184011 import preparation, training, predict
 
+        datasetpath = 'Chapter01/dataset/company_data.csv'
+        # testing function preparation
+        d_train_att, d_train_pass, d_test_att, d_test_pass, d_att, d_pass = preparation(datasetpath)
+        #testing function training
+        t = training(d_train_att, d_train_pass)
+        #testing function testing
+        hasiltestingsemua = testing(t, d_test_att)
+        #hasil
+        print('\n hasil testing : ')
+        print(hasiltestingsemua)
+        ambilsatuhasiltesting = hasiltestingsemua[0]
+        self.assertLessEqual(ambilsatuhasiltesting, 1)
